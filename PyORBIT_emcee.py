Several fixes on RV computation To be done:  - Tcent fit is somehow screwed  - dataset.name_ref not consistent across the program

PyORBIT_emcee.py
@@ -14,7 +14,7 @@ def pyde_save_to_pickle(mc, population, starting_point, prefix=''):
     add_prefix = (prefix + '_' if prefix else '')
     pickle.dump(mc, open(mc.pyde_dir_output + add_prefix + "model_container.p", "wb"))
     pickle.dump(population, open(mc.pyde_dir_output + add_prefix + "population.p", "wb"))
-    pickle.dump(starting_point, open(mc.emcee_dir_output + add_prefix + "starting_point.p", "wb"))
+    pickle.dump(starting_point, open(mc.pyde_dir_output + add_prefix + "starting_point.p", "wb"))
 
 
 def pyde_load_from_cpickle(pyde_dir_output, prefix=''):
@@ -39,7 +39,9 @@ def emcee_save_to_cpickle(mc, starting_point, population, prob, state, sampler, 
     pickle.dump(population, open(mc.emcee_dir_output + add_prefix + "starting_population.p", "wb"))
     pickle.dump(prob, open(mc.emcee_dir_output + add_prefix + "prob.p", "wb"))
     pickle.dump(state, open(mc.emcee_dir_output + add_prefix + "state.p", "wb"))
-    pickle.dump(sampler, open(mc.emcee_dir_output + add_prefix + "sampler.p", "wb"))
+    pickle.dump(sampler.chain, open(mc.emcee_dir_output + add_prefix + "sampler_chain.p", "wb"))
+    pickle.dump(sampler.lnprobability, open(mc.emcee_dir_output + add_prefix + "sampler_lnprobability.p", "wb"))
+    pickle.dump(sampler.acceptance_fraction, open(mc.emcee_dir_output + add_prefix + "sampler_acceptance_fraction.p", "wb"))
 
 
 def emcee_load_from_cpickle(emcee_dir_output, prefix=''):
@@ -49,11 +51,26 @@ def emcee_load_from_cpickle(emcee_dir_output, prefix=''):
     mc = pickle.load(open(emcee_dir_output + add_prefix + "model_container.p", "rb"))
     starting_point = pickle.load(open(emcee_dir_output + add_prefix + "starting_point.p", "rb"))
     population = pickle.load(open(emcee_dir_output + add_prefix + "starting_population.p", "rb"))
-    prob = pickle.dump(open(mc.emcee_dir_output + add_prefix + "prob.p", "rb"))
-    state = pickle.dump(open(mc.emcee_dir_output + add_prefix + "state.p", "rb"))
-    sampler = pickle.load(open(emcee_dir_output + add_prefix + "sampler.p", "rb"))
+    prob = pickle.load(open(mc.emcee_dir_output + add_prefix + "prob.p", "rb"))
+    state = pickle.load(open(mc.emcee_dir_output + add_prefix + "state.p", "rb"))
+    sampler_chain = pickle.load(open(emcee_dir_output + add_prefix + "sampler_chain.p", "rb"))
+    sampler_lnprobability = pickle.load(open(emcee_dir_output + add_prefix + "sampler_lnprobability.p", "rb"))
+    sampler_acceptance_fraction = pickle.load(open(emcee_dir_output + add_prefix + "sampler_acceptance_fraction.p", "rb"))
 
-    return mc, starting_point, population, prob, state, sampler
+    return mc, starting_point, population, prob, state, \
+            sampler_chain, sampler_lnprobability, sampler_acceptance_fraction
+
+
+def emcee_flatchain(chain, nburnin, nthin):
+    """flattening of the emcee chains with removal of burn-in"""
+    _, d, _ = np.shape(chain)
+    nburn = nburnin / nthin
+    if nburn > d:
+        nburn = d/4
+
+    s = chain[:, nburn:, :].shape
+    return chain[:, nburn:, :].reshape(s[0] * s[1], s[2])
+
 
 
 def pyorbit_emcee(config_in):
@@ -72,13 +89,14 @@ def pyorbit_emcee(config_in):
         pass
 
     try:
-        mc, starting_point, population, sampler = emcee_load_from_cpickle(emcee_dir_output, prefix='MR')
+        mc, starting_point, population, _, _, sampler_chain, _, _ = \
+            emcee_load_from_cpickle(emcee_dir_output, prefix='MR')
         reloaded_emcee_multirun = True
     except:
         pass
 
     try:
-        mc, starting_point, population, sampler = emcee_load_from_cpickle(emcee_dir_output, prefix='')
+        mc, starting_point, population, _, _, sampler_chain, _, _ = emcee_load_from_cpickle(emcee_dir_output)
         reloaded_emcee = True
     except:
         pass
@@ -91,7 +109,8 @@ def pyorbit_emcee(config_in):
 
     if reloaded_emcee:
         """ There's no need to do anything"""
-        mc.results_resumen(population)
+        flatchain = emcee_flatchain(sampler_chain, mc.emcee_parameters['nburn'], mc.emcee_parameters['thin'])
+        mc.results_resumen(flatchain)
         return
 
     reloaded_mc = reloaded_pyde or reloaded_emcee_multirun or reloaded_emcee_multirun
@@ -168,7 +187,8 @@ def pyorbit_emcee(config_in):
             sampler = emcee.EnsembleSampler(mc.emcee_parameters['nwalkers'], mc.ndim, mc,
                                             threads=mc.emcee_parameters['nwalkers'])
             population, prob, state = sampler.run_mcmc(population, mc.emcee_parameters['multirun'])
-            mc.results_resumen(population)
+            flatchain = emcee_flatchain(sampler.chain, mc.emcee_parameters['nburn'], mc.emcee_parameters['thin'])
+            mc.results_resumen(flatchain)
 
             max_ind = np.argmax(prob)
             starting_point = population[max_ind, :]
@@ -178,7 +198,9 @@ def pyorbit_emcee(config_in):
             emcee_save_to_cpickle(mc, starting_point, population, prob, state, sampler, prefix='MR_'+repr(ii))
 
         emcee_save_to_cpickle(mc, starting_point, population, prob, state, sampler, prefix='MR')
-        mc.results_resumen(population)
+
+        flatchain = emcee_flatchain(sampler.chain, mc.emcee_parameters['nburn'], mc.emcee_parameters['thin'])
+        mc.results_resumen(flatchain)
 
         print 'emcee exploratory runs completed'
 
@@ -196,15 +218,19 @@ def pyorbit_emcee(config_in):
             emcee_save_to_cpickle(mc, starting_point, population, prob, state, sampler, samples=sampled)
 
             print sampled, '  steps completed, average lnprob:, ', np.median(prob)
-            mc.results_resumen(population)
+
+            flatchain = emcee_flatchain(sampler.chain, mc.emcee_parameters['nburn'], mc.emcee_parameters['thin'])
+            mc.results_resumen(flatchain)
 
     else:
         population, prob, state = sampler.run_mcmc(population, mc.emcee_parameters['nsteps'], thin=mc.emcee_parameters['thin'])
+
         emcee_save_to_cpickle(mc, starting_point, population, prob, state, sampler)
-        mc.results_resumen(population)
+
+        flatchain = emcee_flatchain(sampler.chain, mc.emcee_parameters['nburn'], mc.emcee_parameters['thin'])
+        mc.results_resumen(flatchain)
 
     print 'emcee completed'
-    print
 
 
 if __name__ == '__main__':

test_compare/PyORBIT_TestModel.py
@@ -102,7 +102,6 @@ def create_test_1planet():
     mod_pl = np.random.normal(y_pla, 2)
 
     trip = np.arange(10, 3, -1)
-    Transit_Time = kp.kepler_Tcent_T0P(P, phase, e, omega) + Tref - trip*P
     Transit_Time = kp.kepler_Tcent_T0P(P, phase, e, omega) + Tref
     print 'Transit Time:', Transit_Time
     print 'transit Time - Tref', Transit_Time - Tref
@@ -118,7 +117,76 @@ def create_test_1planet():
 
     fileout = open('test_1planet_Tcent_0.dat', 'w')
     for ii in xrange(0, np.size(Transit_Time)):
-        fileout.write('{0:14f} {1:14f} \n'.format(Transit_Time, 0.01))
+        fileout.write('{0:6d} {1:14f} {2:14f} \n'.format(ii, Transit_Time, 0.01))
     fileout.close()
 
-create_test_1planet()
+#create_test_1planet()
+
+def create_test_1planet_GP():
+
+    import george
+    x = np.arange(6000, 6200, 1., dtype=np.double)
+    x = np.random.normal(x, 0.4)
+    Tref = np.mean(x, dtype=np.double)
+    x0 = x - Tref
+
+    err = x*0.0 + 2.0
+
+
+
+    print Tref
+    P = 23.4237346
+    K = 43.47672
+    phase = 0.34658203
+    e = 0.13
+    omega = 0.673434
+    offset = 0 #45605
+
+    trip = np.arange(-5, 6, 1)
+    Transit_Time = kp.kepler_Tcent_T0P(P, phase, e, omega) + Tref + P*trip
+    print Transit_Time
+
+    y_pla = kp.kepler_RV_T0P(x0, phase, P, K, e, omega) + offset
+
+    input_pams = {'Prot': 17.37462, 'Pdec': 50.93840, 'Oamp': 0.40, 'Hamp': 50.03 }
+
+    gp_pams = np.zeros(4)
+    gp_pams[0] = np.log(input_pams['Hamp'])*2
+    gp_pams[1] = np.log(input_pams['Pdec'])*2
+    gp_pams[2] = 1. / (2*input_pams['Oamp'] ** 2)
+    gp_pams[3] = np.log(input_pams['Prot'])
+
+
+    kernel = np.exp(gp_pams[0]) * \
+                      george.kernels.ExpSquaredKernel(metric=np.exp(gp_pams[1])) * \
+                      george.kernels.ExpSine2Kernel(gamma=gp_pams[1], log_period=gp_pams[2])
+    gp = george.GP(kernel, solver=george.HODLRSolver, seed=42)
+    #gp = george.GP(kernel)
+    gp.compute(x0, err)
+    pred = gp.sample(x)
+
+    mod_pl = np.random.normal(y_pla+pred, 2)
+
+    plt.plot(x, y_pla, c='r')
+    plt.plot(x, pred, c='b')
+    plt.scatter(x, mod_pl, c='y')
+
+    vert_lines = np.arange(-50.0, 50.0, input_pams['Prot'])+Tref
+    for v in vert_lines:
+        plt.axvline(v, c='b')
+
+    for v in Transit_Time:
+        plt.axvline(v, c='r')
+    plt.show()
+
+    fileout = open('test_1planet_GP_RV.dat', 'w')
+    for ii in xrange(0, np.size(x)):
+        fileout.write('{0:14f} {1:14f} {2:14f} {3:5d} {4:5d} {5:5d} \n'.format(x[ii], mod_pl[ii], 2., 0, 0, -1))
+    fileout.close()
+
+    fileout = open('test_1planet_GP_Tcent_0.dat', 'w')
+    for ii in xrange(0, np.size(Transit_Time)):
+        fileout.write('{0:6d} {1:14f} {2:14f} \n'.format(ii, Transit_Time[ii], 0.01))
+    fileout.close()
+
+create_test_1planet_GP()