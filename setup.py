20843189959b07c908d7eaedb18ebbedaae4a612
LucaMalavolta <sWpsN7yNysYM>
2020-04-18 17:38:33 +0200

    Bugfix in setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
 from distutils.core import setup
 from Cython.Build import cythonize
+import sys
 
 setup(
     name="pyorbit",
     author="Luca Malavolta",
-    ext_modules = cythonize("./pyorbit/*/*.pyx"),
-    language_level="3"
+    ext_modules = cythonize("./pyorbit/*/*.pyx", compiler_directives={'language_level' : sys.version_info[0]}),
 )
