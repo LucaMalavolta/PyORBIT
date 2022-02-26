4016f61d259eaaadfc3894cc43edda4cdda77080
Luca Malavolta <luca.malavolta@unipd.it>
2022-02-26 19:47:28 +0100

    Bugfix in CHEOPS detrenging

--- pyorbit/models/cheops_detrending.pyx
+++ pyorbit/models/cheops_detrending.pyx
@@ -42,7 +42,7 @@ class CheopsDetrending(AbstractModel):
 
         self.cheops_diagnostics = {
             'roll_angle': 'None',
-            'ramp': 'max', # ??? 
+            'ramp': 'max', # ???
             'smear': 'max',
             'deltaT': 'None',
             'xoff': 'range',
@@ -77,11 +77,11 @@ class CheopsDetrending(AbstractModel):
 
         for diag_name, diag_scale in self.cheops_diagnostics.items():
             if diag_name in dataset.ancillary.dtype.names:
-                if diag_scale is 'max':
+                if diag_scale == 'max':
                     self.cheops_instrumental[dataset.name_ref][diag_name] = \
                         (dataset.ancillary[diag_name] - np.amin(dataset.ancillary[diag_name])) \
                             / np.ptp(dataset.ancillary[diag_name])
-                elif diag_scale is 'range':
+                elif diag_scale == 'range':
                     self.cheops_instrumental[dataset.name_ref][diag_name] = \
                         ( 2 * dataset.ancillary[diag_name]
                         - (np.amin(dataset.ancillary[diag_name]) + np.amax(dataset.ancillary[diag_name]))) \
@@ -95,7 +95,7 @@ class CheopsDetrending(AbstractModel):
             self.cheops_interpolated[dataset.name_ref][diag_name] =interp1d(
                 dataset.ancillary['time'],
                 self.cheops_instrumental[dataset.name_ref][diag_name],
-                  bounds_error=False, 
+                  bounds_error=False,
                   fill_value=(self.cheops_instrumental[dataset.name_ref][diag_name][0],self.cheops_instrumental[dataset.name_ref][diag_name][-1]))
 
 
@@ -104,8 +104,8 @@ class CheopsDetrending(AbstractModel):
         if x0_input is None:
 
             dt = dataset.x - np.median(dataset.x)
-            # average model is zero - it is added to the light curve 
-            trend = variable_value['dfdt'] *dt +  variable_value['d2fdt2'] *dt**2 
+            # average model is zero - it is added to the light curve
+            trend = variable_value['dfdt'] *dt +  variable_value['d2fdt2'] *dt**2
 
             trend += variable_value['dfdbg']* self.cheops_instrumental[dataset.name_ref]['bg']
 
@@ -113,15 +113,20 @@ class CheopsDetrending(AbstractModel):
 
             trend += variable_value['dfdsmear']* self.cheops_instrumental[dataset.name_ref]['smear']
 
-            trend += self.cheops_instrumental[dataset.name_ref]['smear'] * self.cheops_instrumental[dataset.name_ref]['deltaT'] /1e6
+            trend += self.cheops_instrumental[dataset.name_ref]['ramp'] * self.cheops_instrumental[dataset.name_ref]['deltaT'] /1e6
 
-            trend += variable_value['dfdx']* self.cheops_instrumental[dataset.name_ref]['xoff'] + variable_value['d2fdx2']*self.cheops_instrumental[dataset.name_ref]['xoff']**2
+            trend += variable_value['dfdx']* self.cheops_instrumental[dataset.name_ref]['xoff'] \
+                + variable_value['d2fdx2']*self.cheops_instrumental[dataset.name_ref]['xoff']**2
 
-            trend += variable_value['dfdy']* self.cheops_instrumental[dataset.name_ref]['yoff'] + variable_value['d2fdy2'] * self.cheops_instrumental[dataset.name_ref]['yoff']**2
-            trend += variable_value['d2fdxdy'] * self.cheops_instrumental[dataset.name_ref]['xoff'] * self.cheops_instrumental[dataset.name_ref]['yoff']
+            trend += variable_value['dfdy']* self.cheops_instrumental[dataset.name_ref]['yoff'] \
+                + variable_value['d2fdy2'] * self.cheops_instrumental[dataset.name_ref]['yoff']**2
+            trend += variable_value['d2fdxdy'] \
+                * self.cheops_instrumental[dataset.name_ref]['xoff'] \
+                * self.cheops_instrumental[dataset.name_ref]['yoff']
 
             if self.fit_roll_angle:
-                trend += variable_value['dfdsinphi']*self.cheops_instrumental[dataset.name_ref]['sinphi'] + variable_value['dfdcosphi']*self.cheops_instrumental[dataset.name_ref]['cosphi']
+                trend += variable_value['dfdsinphi']*self.cheops_instrumental[dataset.name_ref]['sinphi'] \
+                    + variable_value['dfdcosphi']*self.cheops_instrumental[dataset.name_ref]['cosphi']
                 trend += variable_value['dfdsin2phi']*(2*self.cheops_instrumental[dataset.name_ref]['sinphi']*self.cheops_instrumental[dataset.name_ref]['cosphi'])
                 trend += variable_value['dfdcos2phi']*(2*self.cheops_instrumental[dataset.name_ref]['cosphi']**2 - 1)
                 trend += variable_value['dfdsin3phi']*(3*self.cheops_instrumental[dataset.name_ref]['sinphi'] - 4* self.cheops_instrumental[dataset.name_ref]['sinphi']**3)
@@ -133,7 +138,7 @@ class CheopsDetrending(AbstractModel):
             t = x0_input + dataset.Tref
             dt = t - np.median(dataset.x)
             # average model is zero - it is added to the light curve
-            trend = variable_value['dfdt'] *dt +  variable_value['d2fdt2'] *dt**2 
+            trend = variable_value['dfdt'] *dt +  variable_value['d2fdt2'] *dt**2
 
             trend += variable_value['dfdbg']* self.cheops_interpolated[dataset.name_ref]['bg'](t)
 
@@ -141,7 +146,7 @@ class CheopsDetrending(AbstractModel):
 
             trend += variable_value['dfdsmear']* self.cheops_interpolated[dataset.name_ref]['smear'](t)
 
-            trend += self.cheops_interpolated[dataset.name_ref]['smear'](t) * self.cheops_interpolated[dataset.name_ref]['deltaT'](t) /1e6
+            trend += self.cheops_interpolated[dataset.name_ref]['ramp'](t) * self.cheops_interpolated[dataset.name_ref]['deltaT'](t) /1e6
 
             trend += variable_value['dfdx']* self.cheops_interpolated[dataset.name_ref]['xoff'](t) + variable_value['d2fdx2']*self.cheops_interpolated[dataset.name_ref]['xoff'](t)**2
 
