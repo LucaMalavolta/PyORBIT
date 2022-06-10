1df6d099acaaf72c64d2388705fcc584ab1ecf84
Luca Malavolta <l.malavolta@gmail.com>
2022-06-10 12:45:18 +0200

    Small update on requirements and docs

--- setup.py
+++ setup.py
@@ -1,19 +1,23 @@
 from setuptools import setup
 import numpy as np
 
+# Inspired from here:
+# https://hynek.me/articles/sharing-your-labor-of-love-pypi-quick-and-dirty/
+
 setup(
     name="pyorbit",
     version='9.0',
     author="Luca Malavolta",
 	author_email = 'luca.malavolta@unipd.it',
 	url = 'https://github.com/LucaMalavolta/PyORBIT',
-	packages =['pyorbit', 'pyorbit.common', 'pyorbit.classes', 'pyorbit.models', 'pyorbit.samplers', 'pyorbit.subroutines'],
+	packages =['pyorbit'],
 	#license = 'GNU GPLv3',
 	#description ='',
 	classifiers = [
 		'Development Status :: 5 - Production/Stable',
 		'Intended Audience :: Science/Research',
 		'Topic :: Scientific/Engineering',
+        'Operating System :: OS Independent',
 		'Programming Language :: Python'
 		],
     # To provide executable scripts, use entry points in preference to the
@@ -25,5 +29,20 @@ setup(
             'pyorbit_results=pyorbit.pyorbit_results:pyorbit_results'
         ]
     },
-     zip_safe=False
+    zip_safe=False
+    install_requires=[
+        'numpy',
+        'numba',
+        'scipy',
+        'matplotlib',
+        'argparse',
+        'emcee',
+        'pyyaml',
+        'h5py',
+        'tqdm',
+        'pygtc',
+        're',
+        'PyDE @ git+https://github.com/hpparvi/PyDE.git'
+    ],
+    setup_requires=['setuptools']
 )
