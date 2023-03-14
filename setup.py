f87df942855e40471f4f0684c48107611ff6720f
Luca Malavolta <l.malavolta@gmail.com>
2023-03-14 16:56:12 +0100

    Added celerite2 SHO term

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ long_description = (this_directory / "README.md").read_text()
 
 setup(
     name="pyorbit-package",
-    version='9.1.8',
+    version='9.1.9',
     author="Luca Malavolta",
 	author_email = 'luca.malavolta@unipd.it',
 	url = 'https://github.com/LucaMalavolta/PyORBIT',
