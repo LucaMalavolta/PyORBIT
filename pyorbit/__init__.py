3c95dd715a46a5d3d7a14c475bb5834df56b995e
Luca Malavolta <luca.malavolta@unipd.it>
2023-07-23 16:02:06 +0200

    Small additions

--- pyorbit/__init__.py
+++ pyorbit/__init__.py
@@ -18,4 +18,4 @@ from .samplers.pyorbit_multinest import *
 from .samplers.pyorbit_getresults import *
 from .subroutines.input_parser import yaml_parser
 
-__version__ = "10.0.0"
+__version__ = "10.0.1"
