1089f93867f98126571e7bcfceaba475da5dc6b3
Luca Malavolta <luca.malavolta@unipd.it>
2024-09-25 15:54:57 +0200

    Small change in pyorbit_results to make plots faster

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ long_description = (this_directory / "README.md").read_text()
 
 setup(
     name="pyorbit-package",
-    version='10.6.1',
+    version='10.6.2',
     author="Luca Malavolta",
 	author_email = 'luca.malavolta@unipd.it',
 	url = 'https://github.com/LucaMalavolta/PyORBIT',
